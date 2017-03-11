b385c6037d121ff29ae7ad823e25bd5c1ac45ee9
intellectape <adi_b09@hotmail.com>
2017-03-11 02:17:33 -0500

    Adding initial files for the project, code not done yet.

---
+++ sac1.R
@@ -0,0 +1,17 @@
+# This project is created by:
+# Name: Aditya Bhardwaj
+# Unity ID: abhardw2
+
+library(igraph)
+
+# Reading the data from the files:
+g <- read.graph(file = "/Users/ADITYA/BI/X/Project6/data/fb_caltech_small_edgelist.txt", format = c("edgelist"))
+attrList <- read.csv("/Users/ADITYA/BI/X/Project6/data/fb_caltech_small_attrlist.csv")
+
+phase1 <- function(){
+  
+}
+
+phase2 <- function(){
+  
+}
